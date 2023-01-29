cc4889be15a2cc6ffb22f5fd6113dacd6536915f
Victor Seleimend <vsennaselei@gmail.com>
2023-01-29 20:18:13 -0300

    Hook useSelectCoins

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,7 @@
 import styled from '@emotion/styled';
 
+import useSelectCoins from '../hooks/useSelectCoins';
+
 const InputSubmit = styled.input`
   background-color: #9497ff;
   border: none;
@@ -19,6 +21,10 @@ const InputSubmit = styled.input`
 `;
 
 const Form = () => {
+  const [SelectCoins] = useSelectCoins();
+
+  SelectCoins();
+
   return (
     <form>
       <InputSubmit type="submit" value="cotar" />
---
+++ src/hooks/useSelectCoins.jsx
@@ -0,0 +1,9 @@
+const useSelectCoins = () => {
+  const SelectCoins = () => {
+    console.log('Seleção de moedas');
+  };
+
+  return [SelectCoins];
+};
+
+export default useSelectCoins;
